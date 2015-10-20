af558874bfe7e833c2ca27f5234aad14d5ebf0dc
Andrew Joia <drewdrewthis@gmail.com>
2015-10-20 19:01:51 -0400

    Adds base classes, ids and js for drag and snap functionality

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+
+    
+
+    $(".no-list, .yes-list").children("li:not(.title)")
+    	// Why does this class need to be hard coded into the html file?
+    	// If it's not, the element isn't snappable.
+    	//.addClass('.ui-widget-header')
+    	.droppable({
+	      activeClass: "ui-state-default",
+	      hoverClass: "ui-state-hover",
+	      drop: function( event, ui ) {
+	        $( this ).addClass( "ui-state-highlight" );
+	      }
+	});
+
+	$(".candidate-list").children("li")
+	.draggable({ 
+		revert: "invalid", 
+		snap: ".ui-widget-header",
+		snapMode: "inner",
+		snapTolerance: 30
+	});
+
+});

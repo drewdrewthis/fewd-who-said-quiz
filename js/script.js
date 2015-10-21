1ec6b1fee565c687e4b7450f5412b3ae8f821448
Andrew Joia <drewdrewthis@gmail.com>
2015-10-20 23:12:04 -0400

    Moves checkRoundOver to out function.

--- js/script.js
+++ js/script.js
@@ -2,21 +2,30 @@ $( document ).ready(function() {
 
     var currentElement = "";
 
+    function checkRoundOver(){
+    	$('.ui-widget-header').each( function( index, element ) {
+    		if($(this).data('candidate'))
+    			console.log($(this).prop('id') + " has someone ");
+    	});
+    }
+
     $(".no-list, .yes-list").children("li:not(.title)")
 	// Why does this class need to be hard coded into the html file?
 	// If it's not, the element isn't snappable.
 	//.addClass('.ui-widget-header')
 	.droppable({
+
 		activeClass: "ui-state-default",
 		hoverClass: "ui-state-hover",
 		drop: function( event, ui ) {
-		$( this ).addClass( "ui-received" )
-			.data("candidate",currentElement);
-			console.log($(this).prop("id") + " " + $(this).data("candidate"));
-		},
+			$( this ).addClass( "ui-received" )
+				.data("candidate",currentElement);
+				console.log($(this).prop("id") + " : " + $(this).data("candidate"));
+			},
 		out: function( event, ui ) {
-			$(this).removeClass( "ui-received" );
-			console.log("removed");
+			$(this).removeClass( "ui-received" )
+				.data("candidate", null);
+				console.log("removed");
 		}
 	});
 
@@ -24,6 +33,9 @@ $( document ).ready(function() {
 	.draggable({ 
 		start: function( event, ui ) {
 			currentElement = $(this).prop('id');
+		},
+		stop: function( event, ui ) {
+			checkRoundOver();
 		},
 		revert: "invalid", 
 		snap: ".ui-widget-header",

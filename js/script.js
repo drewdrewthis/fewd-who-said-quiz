7d2a571fb763601f83152badb13e4d1b944dd9cd
Andrew Joia <drewdrewthis@gmail.com>
2015-10-20 22:29:28 -0400

    Fixes removeClass typo for out function

--- js/script.js
+++ js/script.js
@@ -3,22 +3,21 @@ $( document ).ready(function() {
     var currentElement = "";
 
     $(".no-list, .yes-list").children("li:not(.title)")
-    	// Why does this class need to be hard coded into the html file?
-    	// If it's not, the element isn't snappable.
-    	//.addClass('.ui-widget-header')
-    	.droppable({
-
-	      activeClass: "ui-state-default",
-	      hoverClass: "ui-state-hover",
-	      drop: function( event, ui ) {
-	        $( this ).addClass( "ui-received" )
-	        .data("candidate",currentElement);
-
-	        console.log($(this).prop("id") + " " + $(this).data("candidate"));
-	      },
-	      out: function( event, ui ) {
-	      	$( this ).removeClass( "ui-recieved" );
-	      }
+	// Why does this class need to be hard coded into the html file?
+	// If it's not, the element isn't snappable.
+	//.addClass('.ui-widget-header')
+	.droppable({
+		activeClass: "ui-state-default",
+		hoverClass: "ui-state-hover",
+		drop: function( event, ui ) {
+		$( this ).addClass( "ui-received" )
+			.data("candidate",currentElement);
+			console.log($(this).prop("id") + " " + $(this).data("candidate"));
+		},
+		out: function( event, ui ) {
+			$(this).removeClass( "ui-received" );
+			console.log("removed");
+		}
 	});
 
 	$(".candidate-list").children("li")
